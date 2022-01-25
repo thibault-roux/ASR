4ccc063124b2beb966d7314455f54ac1cf45c075
Thibault Bañeras Roux <59691321+thibault-roux@users.noreply.github.com>
2022-01-25 15:08:02 +0100

    Add files via upload

--- Expes_papier/make_datasets.py
+++ Expes_papier/make_datasets.py
@@ -1,4 +1,4 @@
-from POS_tag import tag
+#from POS_tag import tag
 
 import argparse
 parser = argparse.ArgumentParser(description="Create dataset")
@@ -6,6 +6,7 @@ parser.add_argument("namef", type=str, help="Path to a test file output")
 parser.add_argument("id", type=str, help="ID (SP, KD, etc...)")
 args = parser.parse_args()
 
+print(args)
 
 """
 Logs file without buffering for instant output
@@ -35,10 +36,12 @@ with open(args.namef, "r", encoding="utf8") as basefile:
     lines = basefile.readlines()
     X = ""
     i = 0
+    j = 0
     while i < len(lines):
+        j += 1
         i += 1
         line = clean(lines[i])
-        X += line + "\t"
+        X += str(j) + "\t" + line + "\t"
         i += 2
         line = clean(lines[i])
         X += line + "\t_\n"
@@ -53,9 +56,11 @@ with open(args.namef, "r", encoding="utf8") as basefile:
     lines = basefile.readlines()
     X = ""
     i = 0
+    j = 0
     while i < len(lines):
+        j += 1
         i += 1
-        line = clean(lines[i])
+        line = str(j) + "\t" + clean(lines[i])
         X += line + "\t"
         i += 1
         line = clean(lines[i])
@@ -74,22 +79,21 @@ nbrLine = 0
 for ligne in f:
     nbrLine += 1
     ligne = ligne.split("\t")
-    errors = ligne[1].split(" ")
+    errors = ligne[2].split(" ")
     Erreur = 0
     for e in errors:
         if e != "=":
             Erreur += 1
     if Erreur != 0:
         nbrErreur += 1
-    txt += ligne[0] + "\t" + str(Erreur) + "\t_\n"
+    txt += str(nbrLine) + "\t" + ligne[1] + "\t" + str(Erreur) + "\t_\n"
 with open("data/" + args.id + "3.txt", "w", encoding="utf8") as file:
     file.write(txt)
 
 
 """---------------SP4/KD4---------------"""
-prin("Creation of " + args.id + "4...")
-tag(args.id)
-#"""OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO       A REMETTRE     OOOOOOOOOOOOOOOOOOOOOOOOO"""
+#prin("Creation of " + args.id + "4...")
+#tag(args.id)
 
 
 """---------------SP5/KD5---------------"""
@@ -99,10 +103,23 @@ fbase= open("data/" + args.id + "1.txt", "r", encoding="utf8")
 rpos = fpos.readlines()
 rbase= fbase.readlines()
 txt = ""
-for i in range(len(rpos)):
-    txt += rpos[i].split("\t")[0] + "\t" + rbase[i].split("\t")[0] + "\t" + rbase[i].split("\t")[1] + "\t_\n"
+i = 0
+j = 0
+while i < len(rpos) or j < len(rbase):
+    i_virtual = rpos[i].split("\t")[0]
+    j_virtual = rbase[j].split("\t")[0]
+    if i_virtual == j_virtual:
+        txt += str(i_virtual) + "\t" + rpos[i].split("\t")[1] + "\t" + rbase[j].split("\t")[1] + "\t" + rbase[j].split("\t")[2] + "\t_\n"
+        i += 1
+        j += 1
+    elif i_virtual < j_virtual:
+        i += 1
+    elif j_virtual < i_virtual:
+        j += 1
+
 with open("data/" + args.id + "5.txt", "w", encoding="utf8") as file:
     file.write(txt)
+
 
 
 """---------------SP6/KD6---------------"""
@@ -112,10 +129,23 @@ ferr = open("data/" + args.id + "2.txt", "r", encoding="utf8")
 rpos = fpos.readlines()
 rerr = ferr.readlines()
 txt = ""
-for i in range(len(rpos)):
-    txt += rpos[i].split("\t")[0] + "\t" + rerr[i].split("\t")[1] + "\t_\n"
+i = 0
+j = 0
+while i < len(rpos) or j < len(rerr):
+    i_virtual = rpos[i].split("\t")[0]
+    j_virtual = rerr[j].split("\t")[0]
+    if i_virtual == j_virtual:
+        txt += str(i_virtual) + "\t" + rpos[i].split("\t")[1] + "\t" + rerr[j].split("\t")[2] + "\t_\n"
+        i += 1
+        j += 1
+    elif i_virtual < j_virtual:
+        i += 1
+    elif j_virtual < i_virtual:
+        j += 1
+
 with open("data/" + args.id + "6.txt", "w", encoding="utf8") as file:
     file.write(txt)
+
 
 """---------------SP7/KD7---------------"""
 prin("Creation of " + args.id + "7...")
